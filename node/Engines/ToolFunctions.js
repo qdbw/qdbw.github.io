2cacec93a910d90a75c4101d33ba79b7f7fff092
0xarch <alanqa-ops@outlook.com>
2024-10-07 01:02:35 +0800

    更新渲染工具JS： makeComfortWrap 支持括号换行

--- node/Engines/ToolFunctions.js
+++ node/Engines/ToolFunctions.js
@@ -4,6 +4,7 @@ import AutoTools from "./AutoTools.js";
 const Data = requestData();
 
 const modelDescriptionMap = new Map;
+const wrappedText = new Map;
 
 function getModelData(typeName) {
     if (modelDescriptionMap.has(typeName)) {
@@ -26,27 +27,39 @@ function getModelData(typeName) {
  * @returns {string}
  */
 function makeComfortWrap(stationName) {
-    if (stationName.length < 6) {
-        return stationName;
+    if(wrappedText.has(stationName)){
+        return wrappedText.get(stationName);
     }
-    if (stationName.length == 6) {
-        if (stationName.indexOf('路') < 3 && stationName.indexOf('路') > 0) {
-            let c = stationName.indexOf('路') + 1;
-            return stationName.slice(0, c) + '<br>' + stationName.slice(c)
-        } else {
-            return stationName.slice(0, 3) + '<br>' + stationName.slice(3)
+    let result = (()=>{
+        if (stationName.length < 6) {
+            return stationName;
         }
-    }
-    let zhan = stationName.indexOf('站');
-    let lu = stationName.indexOf('路');
-    if (zhan > 0 && zhan != stationName.length - 1) {
-        return stationName.slice(0, zhan) + '<br>' + stationName.slice(zhan)
-    }
-    if (lu > 0 && lu != stationName.length - 1) {
-        return stationName.slice(0, ++lu) + '<br>' + stationName.slice(lu)
-    }
-    let c = Math.floor(stationName.length / 2);
-    return stationName.slice(0, c) + '<br>' + stationName.slice(c)
+        if (stationName.length == 6) {
+            if ( stationName.indexOf('路') > 0 && stationName.indexOf('路') < 3) {
+                let c = stationName.indexOf('路') + 1;
+                return stationName.slice(0, c) + '<br>' + stationName.slice(c)
+            } else {
+                return stationName.slice(0, 3) + '<br>' + stationName.slice(3)
+            }
+        }
+        if(stationName.indexOf('(') > 0 && stationName.indexOf('(') < 7){
+            console.log(`DETECTED QUOTE ON TEXT(${stationName})`);
+            let c = stationName.indexOf('(');
+            return stationName.slice(0,c) + '<br>' + stationName.slice(c);
+        }
+        let zhan = stationName.indexOf('站');
+        let lu = stationName.indexOf('路');
+        if (zhan > 0 && zhan != stationName.length - 1) {
+            return stationName.slice(0, zhan) + '<br>' + stationName.slice(zhan)
+        }
+        if (lu > 0 && lu < stationName.length - 1) {
+            return stationName.slice(0, ++lu) + '<br>' + stationName.slice(lu)
+        }
+        let c = Math.floor(stationName.length / 2);
+        return stationName.slice(0, c) + '<br>' + stationName.slice(c)
+    })();
+    wrappedText.set(stationName,result);
+    return result;
 }
 
 /**
