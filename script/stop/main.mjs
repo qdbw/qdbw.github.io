8c9d117a03d8f44f37d61b8e0b93b86f2c54ddc7
0xarch <alanqa-ops@outlook.com>
2025-04-15 17:58:09 +0800

    fix error in build script

--- script/stop/main.mjs
+++ script/stop/main.mjs
@@ -4,56 +4,6 @@ import { readFile, readdir, stat, mkdir, writeFile } from "fs/promises";
 import { basename, join } from "path";
 import { compile } from "pug";
 
-export const StopUtils = {
-    generateHistoryLineName(current_line_name, history_tag) {
-        return `H~${current_line_name}@${history_tag}`;
-    },
-    /**
-     * 
-     * @param {LineInfoContainer} current_line 
-     */
-    async assignHistoryLine(current_line) {
-        let tasks = [];
-        for (let history_tag of current_line.histories_stringlist) {
-            tasks.push((async () => {
-                let history_data_dir = join(current_line.data_path, 'Histories', history_tag);
-                let history_line_name = LineUtils.generateHistoryLineName(current_line.name, history_tag);
-                let history_line = await LineUtils.createInfoFromPath(history_line_name, history_data_dir, true, history_tag, current_line);
-                current_line.histories[current_line.histories_stringlist.indexOf(history_tag)] = history_line;
-            })());
-        }
-        await Promise.all(tasks);
-    },
-    /**
-     * 
-     * @param {LineInfoContainer} current_line 
-     */
-    async assignRoute(current_line) {
-        let tasks = [];
-        for (let route_tag of current_line.routes_stringlist) {
-            tasks.push((async () => {
-                let route_data_path = join(current_line.data_path, `Route.${route_tag}.jsonc`);
-                let route_data = BUtil.JSON.safeParse((await readFile(route_data_path)).toString());
-                current_line.routes[current_line.routes_stringlist.indexOf(route_tag)] = new RouteInfoContainer(route_tag, route_data);
-            })());
-        }
-        await Promise.all(tasks);
-    },
-    async createInfoFromPath(name, data_path, ...rest) {
-        let content = (await readFile(join(data_path, 'Main.jsonc'))).toString();
-        let json = BUtil.JSON.safeParse(content);
-        if (json instanceof Error) {
-            return new LineInfoContainer(name, undefined, data_path);
-        }
-        let result = new LineInfoContainer(name, json, data_path, ...rest);
-        await Promise.all([
-            LineUtils.assignHistoryLine(result),
-            LineUtils.assignRoute(result)
-        ]);
-        return result;
-    }
-}
-
 export class StopPageBuilder {
     data_dir;
     public_dir;
@@ -76,7 +26,7 @@ export class StopPageBuilder {
             }
         };
         let target_directory = join(this.public_dir, "stop");
-        let target_path = join(target_directory, `${object.name}.html`);
+        let target_path = join(target_directory, `${object.id}.html`);
         await mkdir(target_directory, { recursive: true });
         await writeFile(target_path, await this.toHtml(objs));
         console.log(`[LOG] Built stop_${object.name}`);
