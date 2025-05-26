c37c24d36b15e2fc31e8fc3c1136cb4d9f082901
0xarch <alanqa-ops@outlook.com>
2025-05-26 20:35:25 +0800

    update util console

--- util/console.mjs
+++ util/console.mjs
@@ -11,16 +11,18 @@
 
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "node:fs";
 import { dirname } from "node:path";
-import { parse, stringify} from "yaml";
+import { parse, stringify } from "yaml";
 
 function getObjectFilePath(location, company, subcompany, id) {
     let prefix = String(id).replace(/[0-9]*/g, '');
     let result = `${location}/${company}/${subcompany}/`;
-    if (prefix != '') {
-        result += `${prefix}/`;
-    }
     let num_id = Number(String(id).replace(prefix, ''));
     let p_from = Math.floor(num_id / 100);
+    if (prefix != '') {
+        result += `${prefix}/`;
+    } else {
+        p_from = String(p_from).padStart(2, '0');
+    }
     result += `${prefix}${p_from}00-${prefix}${p_from}99/${location}-${company}-${subcompany}-${id}.yaml`;
     return result;
 }
@@ -61,6 +63,7 @@ switch (first_selector) {
         let from = Number(String(process.argv[7]).replace(prefix, ''));
         let to = Number(String(process.argv[9]).replace(prefix, ''));
         for (let i = from; i <= to; i++) {
+            if (prefix == '') i = String(i).padStart(4, '0');
             objectives.push(`${prefix}${i}`);
         }
         break;
@@ -68,16 +71,17 @@ switch (first_selector) {
         let range_content = process.argv[7].split(",");
         console.log(range_content);
         range_content.forEach(v => {
-            if(v.match(/^[A-Z]*?[0-9]+$/)){
+            if (v.match(/^[A-Z]*?[0-9]+$/)) {
                 objectives.push(v);
                 return;
             }
-            if(v.split("-",2).every(v => v.match(/^[A-Z]*?[0-9]+$/))) {
-                let [v_from,v_to] = v.split("-",2);
+            if (v.split("-", 2).every(v => v.match(/^[A-Z]*?[0-9]+$/))) {
+                let [v_from, v_to] = v.split("-", 2);
                 let prefix = String(v_from).replace(/[0-9]*/g, '');
                 let from = Number(String(v_from).replace(prefix, ''));
                 let to = Number(String(v_to).replace(prefix, ''));
                 for (let i = from; i <= to; i++) {
+                    if (prefix == '') i = String(i).padStart(4, '0');
                     objectives.push(`${prefix}${i}`);
                 }
             }
@@ -95,17 +99,17 @@ let objective_bypath = objectives.map(v => 'data/bus/' + getObjectFilePath(locat
 // console.log(`Objectives really:`, objective_bypath);
 
 objective_bypath.forEach(v => {
-    mkdirSync(dirname(v), {recursive: true});
-    if(!existsSync(v)){
-        writeFileSync(v,'');
+    mkdirSync(dirname(v), { recursive: true });
+    if (!existsSync(v)) {
+        writeFileSync(v, '');
     }
 });
 
-if(operation === 'modify') {
+if (operation === 'modify') {
     let modify_type = process.argv[operation_detail_begin];
     console.log('Modify type:', modify_type);
-    if(modify_type === 'model'){
-        let model_type = process.argv[operation_detail_begin+1];
+    if (modify_type === 'model') {
+        let model_type = process.argv[operation_detail_begin + 1];
         objective_bypath.forEach(v => {
             let raw_content = readFileSync(v).toString();
             let yaml_content = parse(raw_content) ?? {};
@@ -113,27 +117,27 @@ if(operation === 'modify') {
             writeFileSync(v, stringify(yaml_content));
         });
     }
-    else if(modify_type === 'history'){
-        let date = process.argv[operation_detail_begin+1];
-        let from = process.argv[operation_detail_begin+2];
-        let to = process.argv[operation_detail_begin+3];
+    else if (modify_type === 'history') {
+        let date = process.argv[operation_detail_begin + 1];
+        let from = process.argv[operation_detail_begin + 2];
+        let to = process.argv[operation_detail_begin + 3];
         console.log(`MODIFY History: From ${from} to ${to} in ${date}`);
         objective_bypath.forEach(v => {
             let raw_content = readFileSync(v).toString();
             let yaml_content = parse(raw_content) ?? {};
-            if(yaml_content.history) yaml_content.history = undefined;
+            if (yaml_content.history) yaml_content.history = undefined;
             yaml_content.shift_records ??= [];
-            for(let record of yaml_content.shift_records){
-                if(record.date === date && record.from === from && record.to === to){
+            for (let record of yaml_content.shift_records) {
+                if (record.date === date && record.from === from && record.to === to) {
                     return;
                 }
             }
             yaml_content.shift_records.push({
                 date, from, to
             });
-            yaml_content.shift_records.sort((a,b) => {
-                let [ya,ma,da] = String(a).split('.');
-                let [yb,mb,db] = String(b).split('.');
+            yaml_content.shift_records.sort((a, b) => {
+                let [ya, ma, da] = String(a).split('.');
+                let [yb, mb, db] = String(b).split('.');
                 // Compare by year, month, day (with optional MM and DD)
                 ya = Number(ya);
                 yb = Number(yb);
